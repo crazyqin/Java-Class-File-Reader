4664dd1a94654f1b18d4afe9db040f8e891849a6
crazyqin <qinyi882@163.com>
2015-03-09 14:04:20 +0800

    function getVersion and getCONSTANT_Pool_count

--- Source.py
+++ Source.py
@@ -9,10 +9,20 @@ def checkMagicNumber(f,begin):
         flag=False
         magic_number, = struct.unpack("!L",c[begin:begin+4])
         magic_number=hex(magic_number)[2:-1]
-        print magic_number
         if magic_number=="cafebabe":
                 flag=True
         return flag,begin+4
+
+def getVersion(f,begin):
+        minor_version, = struct.unpack("!H",c[begin:begin+2])
+        major_version, = struct.unpack("!H",c[begin+2:begin+4])
+        return minor_version,major_version,begin+4
+
+def getCONSTANT_Pool_count(f,begin):
+        CONSTANT_Pool_Count, = struct.unpack("!H",c[begin:begin+2])
+        return CONSTANT_Pool_Count
+
+
 
 if __name__=="__main__":
         #filepath=raw_input("Please input the filepath:")
@@ -28,6 +38,12 @@ if __name__=="__main__":
         if flag==False:
                 print "Not a Java Class File!"
                 sys.exit()
-        print "Java Class File detected!"
+        print "Java Class File detected!(cafebabe)"
+
+        minor_version,major_version,fPointer = getVersion(f,fPointer)
+        print "minor_version:",minor_version,"major_version:",major_version
+
+        CONSTANT_Pool_Count = getCONSTANT_Pool_count(f,fPointer)
+        print "CONSTANT_Pool_Count:",CONSTANT_Pool_Count
 
         
