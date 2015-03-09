d4b70beb3b5e7a3aa731d8b34746a61d29e6d171
crazyqin <qinyi882@163.com>
2015-03-09 13:29:53 +0800

    function checkMagicNumber

--- Source.py
+++ Source.py
@@ -5,15 +5,29 @@ import sys
 import struct
 import binascii
 
-#filepath=raw_input("Please input the filepath:")
-filepath=r'helloworld.class'
-try:
-	f=open(filepath,'rb')
-	c=f.read()
-	f.close()
-except Exception, e:
-	print 'Unable to open the file.'
-	sys.exit()
-print c[0:4]
-#magic_number=struct.unpack("I",c[0:4])
-#print magic_number
+def checkMagicNumber(f,begin):
+        flag=False
+        magic_number, = struct.unpack("!L",c[begin:begin+4])
+        magic_number=hex(magic_number)[2:-1]
+        print magic_number
+        if magic_number=="cafebabe":
+                flag=True
+        return flag,begin+4
+
+if __name__=="__main__":
+        #filepath=raw_input("Please input the filepath:")
+        filepath=r"helloworld.class"
+        try:
+                f=open(filepath,"rb")
+                c=f.read()
+                f.close()
+        except Exception, e:
+                print "Unable to open the file."
+                sys.exit()
+        flag,fPointer=checkMagicNumber(f,0)
+        if flag==False:
+                print "Not a Java Class File!"
+                sys.exit()
+        print "Java Class File detected!"
+
+        
